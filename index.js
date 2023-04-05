988e2407ac3d11c13d9b8ddf5cfdf09003ad0f79
AidenMiller123 <123018143+AidenMiller123@users.noreply.github.com>
2023-04-04 20:56:53 -0400

    Fixed inputs

--- index.js
+++ index.js
@@ -5,63 +5,35 @@ inquirer
 // Array of question the user will be asked
 .prompt ([{
     type: 'input',
-    message: 'What is your GitHub username?',
-    name: 'github',
-  },
-  {
-      type: 'input',
-      message: 'What is your email address?',
-      name: 'email',
+    message: 'What would you like the text to be? Max 3 characters',
+    name: 'text',
     },
     {
-      type: 'input',
-      message: 'What is your project name?',
-      name: 'title',
+    type: 'input',
+    message: 'What color would you like your text?',
+    name: 'textColor',
     },
     {
-      type: 'input',
-      message: 'Please write a short description of your project',
-      name: 'description',
+    type: 'list',
+    name: 'shape',
+    message: 'What kind of shape would you like your logo to have?',
+    choices: [
+          { title: 'circle', value: 'circle' },
+          { title: 'triangle', value: 'triangle'},
+          { title: 'square', value: 'square'},
+            ],
+    initial: 1
     },
     {
-        type: 'list',
-        name: 'license',
-        message: 'What kind of licnese should your project have?',
-        choices: [
-          { title: 'Apache 2.0', value: 'Apache 2.0' },
-          { title: 'GNU GPL v3', value: 'GNU GPL v3'},
-          { title: 'The Perl License', value: 'The Perl License'},
-          { title: 'ISC License (ISC)', value: 'ISC License (ISC)'},
-          { title: 'The MIT License', value: 'The MIT License' },
-        ],
-        initial: 1
-      },
-      {
-        type: 'input',
-        message: 'What command should be run to install dependencies?',
-        name: 'install',
-      },
-      {
-        type: 'input',
-        message: 'What command should be run to run test?',
-        name: 'tests',
-      },
-      {
-        type: 'input',
-        message: 'What does the user need to know about using the repo?',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'What does the user need to know about contributing to the repo?',
-        name: 'contributing',
-      },
-    
+    type: 'input',
+    message: 'What color would you like your shape?',
+    name: 'shapeColor',
+    },
 ])
 .then((data) => {
    logoPageContent = generateLogo(data);
 
-    fs.writeFile('logo.svg', logoPageContent, (err) =>
-     err ? console.error(err) : console.log('Your Readme file was succesfully made!'))
+    fs.writeFile(`logo.svg`, logoPageContent, (err) =>
+     err ? console.error(err) : console.log('Generated logo.svg'))
     
   })
